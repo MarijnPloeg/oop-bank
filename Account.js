fdb5ba0603347cafc584e2547fdcecd1f819464e
Marijn Ploeg <marijnploeg@gmail.com>
2021-02-18 15:22:04 +0100

    passed all tests

--- Account.js
+++ Account.js
@@ -1,2 +1,26 @@
 // Export your class here as module.exports = MyClass
-module.exports = null;
+module.exports = Account;
+
+function Account(name, balance, creditLimit) {
+    this.name = name;
+    this.balance = balance;
+    this.creditLimit = creditLimit;
+
+    this.makeDeposit = function makeDeposit(deposit) {
+        if (deposit <= 0) {
+            return this.balance
+        }
+        this.balance = this.balance + deposit;
+
+    }
+
+    this.makeWithdrawal = function makeWithdrawal(withdrawal) {
+        if (withdrawal > this.balance + this.creditLimit) {
+            return this.balance
+        }
+        else if (withdrawal > 5000) {
+            return this.balance;
+        }
+        this.balance = this.balance - withdrawal;
+    }
+}
--- BusinessAccount.js
+++ BusinessAccount.js
@@ -1,2 +1,30 @@
 // Export your class here as module.exports = MyClass
-module.exports = null;
+const Account = require("./Account");
+
+class BusinessAccount extends Account {
+    constructor(name, balance, creditLimit, sepaPermission) {
+        super(name, balance, creditLimit);
+        this.sepaPermission = sepaPermission;
+
+        this.makeWithdrawal = function makeWithdrawal(withdrawal) {
+            if (withdrawal > this.balance + this.creditLimit) {
+                return this.balance
+            }
+            else if (withdrawal > 20000) {
+                return this.balance;
+            }
+            this.balance = this.balance - withdrawal;
+        }
+
+        this.sepaInvoice = function sepaInvoice(invoiceAmount) {
+            if (invoiceAmount >= 0 && sepaPermission === true) {
+                const transactionCost = invoiceAmount / 100;
+                this.balance = (balance + invoiceAmount) - transactionCost;
+            } else {
+                return this.balance;
+            }
+        }
+    }
+}
+
+module.exports = BusinessAccount;
